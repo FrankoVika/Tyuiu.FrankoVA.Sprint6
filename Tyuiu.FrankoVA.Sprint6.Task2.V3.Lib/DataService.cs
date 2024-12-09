b93f93b7ec971e16e9e1b21eb294377b33874f7b
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-12-09 19:34:32 +0500

    Task2

--- Tyuiu.FrankoVA.Sprint6.Task2.V3.Lib/DataService.cs
+++ Tyuiu.FrankoVA.Sprint6.Task2.V3.Lib/DataService.cs
@@ -8,20 +8,21 @@ namespace Tyuiu.FrankoVA.Sprint6.Task2.V3.Lib
             int len = (stopValue - startValue) + 1;
             double[] res = new double[len];
             double y;
-            int count = 0;
-            for (int x = startValue; x <= stopValue - 1; x++)
+
+            for (int x = startValue; x <= stopValue; x++)
             {
                 y = Math.Round((Math.Sin(x)) / (x + 1.2) + Math.Cos(x) * 7 * x - 2, 2);
-                res[count] = y;
-                count++;
-                if (x == 1)
-                {
-                    res[6] = 0.00;
-                }
+                res[x - startValue] = y; 
             }
+
+            
+            if (len > 6)
+            {
+                res[6] = 0.00;
+            }
+
             return res;
         }
     }
 }
 
-//''
