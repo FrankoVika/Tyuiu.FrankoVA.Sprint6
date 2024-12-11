20fd10747bb3d5bfde2d7234721d28d0f0ee31a1
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-12-11 14:41:01 +0500

    Task5

---
+++ Tyuiu.FrankoVA.Sprint6.Task5.V22/FormMain.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Tyuiu.FrankoVA.Sprint6.Task5.V22
+{
+    partial class FormMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            button1 = new Button();
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            groupBox2 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            buttonChek = new Button();
+            buttonInfo = new Button();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(650, 36);
+            button1.Name = "button1";
+            button1.Size = new Size(132, 48);
+            button1.TabIndex = 16;
+            button1.Text = "Выполнить";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(0, 1);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(598, 64);
+            groupBox1.TabIndex = 14;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Условие";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 23);
+            label2.Name = "label2";
+            label2.Size = new Size(209, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Результат вывести в таблице";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(dataGridView1);
+            groupBox2.Location = new Point(0, 96);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(345, 477);
+            groupBox2.TabIndex = 17;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Вывод данных";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 26);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(249, 445);
+            dataGridView1.TabIndex = 0;
+            // 
+            // buttonChek
+            // 
+            buttonChek.Location = new Point(927, 36);
+            buttonChek.Name = "buttonChek";
+            buttonChek.Size = new Size(130, 48);
+            buttonChek.TabIndex = 19;
+            buttonChek.Text = "Сохранить";
+            buttonChek.UseVisualStyleBackColor = true;
+            buttonChek.Click += button3_Click;
+            // 
+            // buttonInfo
+            // 
+            buttonInfo.Location = new Point(788, 36);
+            buttonInfo.Name = "buttonInfo";
+            buttonInfo.Size = new Size(133, 48);
+            buttonInfo.TabIndex = 18;
+            buttonInfo.Text = "Справка";
+            buttonInfo.UseVisualStyleBackColor = true;
+            buttonInfo.Click += button2_Click;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(365, 96);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(664, 497);
+            chart1.TabIndex = 20;
+            chart1.Text = "chart1";
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1090, 605);
+            Controls.Add(chart1);
+            Controls.Add(button1);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBox2);
+            Controls.Add(buttonChek);
+            Controls.Add(buttonInfo);
+            Name = "FormMain";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private GroupBox groupBox1;
+        private Label label2;
+        private GroupBox groupBox2;
+        private Button buttonChek;
+        private Button buttonInfo;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private DataGridView dataGridView1;
+    }
+}
